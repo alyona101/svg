0f5a2abf0f2fed7cc727da6172ab1ccf659d86ad
artemqdh <artjom-05@mail.ru>
2024-05-12 13:37:54 +0300

    Text update 1

--- svg.cpp
+++ svg.cpp
@@ -32,4 +32,40 @@ void Circle::RenderObject(const RenderContext& context) const {
     out << "/>"sv;
 }
 
+Text& Text::SetPosition(Point pos)
+{
+    pos_ = pos;
+    return *this;
+}
+
+Text& Text::SetOffset(Point offset)
+{
+    offset_ = offset;
+    return *this;
+}
+
+Text& Text::SetFontSize(uint32_t size)
+{
+    font_size = size;
+    return *this;
+}
+
+Text& Text::SetFontFamily(std::string font_family)
+{
+    font_family_ = font_family;
+    return *this;
+}
+
+Text& Text::SetFontWeight(std::string font_weight)
+{
+    font_weight_ = font_weight;
+    return *this;
+}
+
+Text& Text::SetData(std::string data)
+{
+    data_ = data;
+    return *this;
+}
+
 }  // namespace svg
